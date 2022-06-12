ff46ed27d69a95e6956a0132b2d9bb8e4952e4bd
Reem Suwaileh <12887670+rsuwaileh@users.noreply.github.com>
2022-06-13 02:55:59 +0300

    added functions

--- TLLMR4CM/__init__.py
+++ TLLMR4CM/__init__.py
@@ -1,4 +1,11 @@
 from . import lmr
-from . import utils
+#from . import utils
 
-from lmr import train, evaluate
+def set_seed(args):
+  lmr.set_seed(args)
+
+def train(args, train_dataset, model, tokenizer, labels, pad_token_label_id):
+  lmr.train(args, train_dataset, model, tokenizer, labels, pad_token_label_id)
+
+def evaluate(args, model, tokenizer, labels, pad_token_label_id, mode, prefix=""):
+  lmr.evaluate(args, model, tokenizer, labels, pad_token_label_id, mode, prefix="")
