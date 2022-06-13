be8c9de4a6f1fb8bca5d96ae2c029493dad92a99
Reem Suwaileh <12887670+rsuwaileh@users.noreply.github.com>
2022-06-13 03:38:46 +0300

    Update __init__.py

--- TLLMR4CM/__init__.py
+++ TLLMR4CM/__init__.py
@@ -1,4 +1,5 @@
-from . import lmr
+from .lmr import set_seed, train, evaluate
+
 #from . import utils
 
 __all__ = [ 'set_seed',
@@ -7,10 +8,11 @@ __all__ = [ 'set_seed',
 
 
 def set_seed(args):
-  lmr.set_seed(args)
+  set_seed(args)
 
 def train(args, train_dataset, model, tokenizer, labels, pad_token_label_id):
-  lmr.train(args, train_dataset, model, tokenizer, labels, pad_token_label_id)
+  train(args, train_dataset, model, tokenizer, labels, pad_token_label_id)
 
 def evaluate(args, model, tokenizer, labels, pad_token_label_id, mode, prefix=""):
-  lmr.evaluate(args, model, tokenizer, labels, pad_token_label_id, mode, prefix="")
+  evaluate(args, model, tokenizer, labels, pad_token_label_id, mode, prefix="")
+
